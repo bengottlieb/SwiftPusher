798c80427fcff08084b18c0e2d51da935da44060
Ben Gottlieb <ben@standalone.com>
2023-02-26 20:23:14 -0600

    improved sending API

--- Sources/SwiftPusher/Peer Discovery/PushTargetDevice.swift
+++ Sources/SwiftPusher/Peer Discovery/PushTargetDevice.swift
@@ -8,11 +8,17 @@
 import Foundation
 import Nearby
 
-public struct PushTargetDevice: Equatable {
+public struct PushTargetDevice: Equatable, Codable {
 	public var apnsToken: String
 	public var name: String
 	
 	public static func ==(lhs: Self, rhs: Self) -> Bool {
 		lhs.apnsToken == rhs.apnsToken
+	}
+}
+
+extension PushTargetDevice {
+	public func push(_ payload: [String: Any], background: Bool = false) async throws {
+		try await Pusher.instance.send(payload: payload, to: apnsToken, background: background)
 	}
 }
--- Sources/SwiftPusher/Pusher.swift
+++ Sources/SwiftPusher/Pusher.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Suite
 
 public class Pusher {
 	public static let instance = Pusher()
@@ -26,6 +27,32 @@ public class Pusher {
 	public var server = Server.sandbox
 	
 	public func send(message: String, body: String? = nil, sound: String? = "default", to token: String) async throws {
+		
+		let payload = APNS(aps: .init(alert: .init(title: message, body: body), sound: sound))
+		try await send(payload: payload.aps.asJSON(), to: token)
+		
+//		assert(!teamID.isEmpty && !bundleID.isEmpty && key != nil, "Please call Pusher.instance.setup(teamID:key:bundleID:) before sending.")
+//
+//		let path = "/3/device/\(token)"
+//		let jwt = try buildJWT()
+//		let headers: [String: String] = [
+//		  "apns-topic": bundleID,
+//		  "authorization": "bearer \(jwt)",
+//		  "Content-Type": "application/json; charset=utf-8",
+//		]
+//		let payload = APNS(aps: .init(alert: .init(title: message, body: body), sound: sound))
+//		let url = server.url.appendingPathComponent(path)
+//		var request = URLRequest(url: url)
+//		let body = try JSONEncoder().encode(payload)
+//		request.httpBody = body
+//		request.httpMethod = "post"
+//		request.allHTTPHeaderFields = headers
+//
+//		let r = try await URLSession.shared.data(for: request)
+//		print(r)
+	}
+
+	public func send(payload aps: [String: Any], to token: String, background: Bool = false) async throws {
 		assert(!teamID.isEmpty && !bundleID.isEmpty && key != nil, "Please call Pusher.instance.setup(teamID:key:bundleID:) before sending.")
 		
 		let path = "/3/device/\(token)"
@@ -34,11 +61,14 @@ public class Pusher {
 		  "apns-topic": bundleID,
 		  "authorization": "bearer \(jwt)",
 		  "Content-Type": "application/json; charset=utf-8",
+		  "apns-push-type": background ? "background" : "alert",
+		  "apns-priority": background ? "5" : "10",
 		]
-		let payload = APNS(aps: .init(alert: .init(title: message, body: body), sound: sound))
+		var apsCopy = aps
+		if background { apsCopy["content-available"] = 1 }
 		let url = server.url.appendingPathComponent(path)
 		var request = URLRequest(url: url)
-		let body = try JSONEncoder().encode(payload)
+		let body = try JSONSerialization.data(withJSONObject: ["aps": apsCopy])
 		request.httpBody = body
 		request.httpMethod = "post"
 		request.allHTTPHeaderFields = headers
@@ -46,6 +76,6 @@ public class Pusher {
 		let r = try await URLSession.shared.data(for: request)
 		print(r)
 	}
-		
+
 }
 
